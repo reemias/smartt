88540c0a52c95ddb560fa3a2e1c102ac28188514
reemias <141964407+reemias@users.noreply.github.com>
2023-08-11 17:47:00 -0300

    commit inicial

---
+++ JS.js
@@ -0,0 +1,17 @@
+ // Função para baixar o passo a passo
+ document.getElementById("baixarPassoAPasso").addEventListener("click", function() {
+    // Coloque aqui a lógica para baixar o passo a passo
+    // Exemplo: window.location.href = "link_do_passo_a_passo.pdf";
+});
+
+// Função para baixar o módulo
+document.getElementById("baixarModulo").addEventListener("click", function() {
+    // Coloque aqui a lógica para baixar o módulo
+    // Exemplo: window.location.href = "link_do_modulo.pdf";
+});
+
+// Função para baixar o currículo
+document.getElementById("baixarCurriculo").addEventListener("click", function() {
+    // Coloque aqui a lógica para baixar o currículo
+    // Exemplo: window.location.href = "link_do_curriculo.pdf";
+});
